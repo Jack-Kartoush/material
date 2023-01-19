d453fcbcdff10580e15f97b3f4c30e07f7043033
Jack Kartosh <jackkartosh@gmail.com>
2023-01-19 10:24:48 +0100

    Formating  en aanpassing van de styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component , HostBinding } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import {
   trigger,
   state,
@@ -11,39 +11,40 @@ import {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  
+
   animations: [
-   
     trigger('openClose', [
       // ...
-      state('open', style({
-        height: '200px',
-        width: '0px',
-        position: 'relative',
-        right: '50%',
-        // opacity:0 ,
-        backgroundColor: 'yellow'
-      })),
-      state('closed', style({
-        position: 'relative',
-        right: '0',
-        // opacity:1 ,
-        backgroundColor: 'blue',
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s')
-      ]),
+      state(
+        'open',
+        style({
+          height: '200px',
+          width: '0px',
+          position: 'relative',
+          right: '100%',
+          // opacity:0 ,
+          backgroundColor: 'yellow',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          position: 'relative',
+          right: '0',
+          // opacity:1 ,
+          backgroundColor: 'blue',
+        })
+      ),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.5s')]),
     ]),
   ],
 })
 export class AppComponent {
   isOpen = true;
+
   
   toggle() {
     this.isOpen = !this.isOpen;
   }
 }
-
